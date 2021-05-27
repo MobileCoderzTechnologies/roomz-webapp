303fed9ca7b06e85a8290568545307ad2a500594
pukhraj saini <pukhraj.saini@mobilecoderz.com>
2021-05-27 19:02:16 +0530

    modefication in translate pipe

--- src/app/modules/translate/pipes/lang-translate.pipe.ts
+++ src/app/modules/translate/pipes/lang-translate.pipe.ts
@@ -14,7 +14,21 @@ export class LangTranslatePipe implements PipeTransform {
 
   transform(key): string {
     if (this.$translate.data) {
-      const translation = this.$translate.data[key];
+      let translation;
+      if (key.includes('.')) {
+        const [first, second, third, fourth, fifth] = key.split('.');
+        if (fifth) {
+          translation = this.$translate.data[first][second][third][fourth][fifth];
+        } else if (fourth) {
+          translation = this.$translate.data[first][second][third][fourth];
+        } else if (third) {
+          translation = this.$translate.data[first][second][third];
+        } else {
+          translation = this.$translate.data[first][second];
+        }
+      } else {
+        translation = this.$translate.data[key];
+      }
       return translation;
     } else {
       return '';
