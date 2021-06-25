ffd36aeb2e794a9e71bf670f0d3848335a91dc97
Santosh kumar <santosh.kumar@mobilecoderz.com>
2021-06-25 10:40:48 +0530

    first commit ui-m2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,40 +3,48 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { TRAVELLING_ROUTE } from './constants/route.constants';
 
-
 const routes: Routes = [
   {
     path: '',
     redirectTo: TRAVELLING_ROUTE.path,
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'travelling',
-    loadChildren: () => import('./pages/travelling/travelling.module').then(m => m.TravellingModule)
+    loadChildren: () =>
+      import('./pages/travelling/travelling.module').then(
+        (m) => m.TravellingModule
+      ),
   },
   {
-    path: 'ar', children: [
+    path: 'ar',
+    children: [
       {
         path: '',
         redirectTo: TRAVELLING_ROUTE.path,
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'travelling',
-        loadChildren: () => import('./pages/travelling/travelling.module').then(m => m.TravellingModule)
+        loadChildren: () =>
+          import('./pages/travelling/travelling.module').then(
+            (m) => m.TravellingModule
+          ),
       },
-    ]
+    ],
   },
   {
     path: '**',
-    component: NotFoundComponent
-  }
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
