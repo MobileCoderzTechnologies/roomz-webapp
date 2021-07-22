e2f08e981419bb50694bd0ab6aa38365bd4f71d4
pukhraj saini <pukhraj@mobilecoderz.com>
2021-07-22 16:53:10 +0530

    routes define

--- src/app/pages/hosting/constansts/route.constant.ts
+++ src/app/pages/hosting/constansts/route.constant.ts
@@ -20,3 +20,81 @@ export const STEP_1_ROUTE = {
     return `${LISTING_HOME_ROUTE.url}/${this.path}`;
   }
 };
+
+export const STEP_2_ROUTE = {
+  path: 'spaces',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+
+export const STEP_3_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+
+export const STEP_4_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_5_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_6_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_7_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_8_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_9_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_10_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+export const STEP_11_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+
+export const STEP_12_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+
+export const STEP_13_ROUTE = {
+  path: '',
+  get url(): string {
+    return `${LISTING_HOME_ROUTE}/${this.path}`;
+  }
+};
+
